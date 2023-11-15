8178e62c2f2e7c4b9aa21c15d72d9aa6b6c9698e
rameshramesh2001 <rameshramesh4426@gmail.com>
2023-11-15 14:08:31 +0530

    singlylinkedlist

---
+++ src/Java_programs/linkedlist.java
@@ -0,0 +1,17 @@
+package Java_programs;
+
+public class linkedlist {
+	 public static void main(String[] args) {    
+         
+	        Singlylinkedlist sList = new Singlylinkedlist();    
+	            
+	        //Add nodes to the list    
+	        sList.addNode(1);    
+	        sList.addNode(2);    
+	        sList.addNode(3);    
+	        sList.addNode(4);    
+	            
+	        //Displays the nodes present in the list    
+	        sList.display();    
+	    }   
+}
